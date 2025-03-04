dbeffca25ec37a4df3037d358684ec0d13147903
Vitor Pinheiro <vycthorpf@yahoo.com.br>
2025-03-04 00:40:57 -0300

    Alterando limite para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,9 +11,9 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
-
+clearInterval
 exibirMensagemInicial();
 
 function verificarChute() {
